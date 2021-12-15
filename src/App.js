9e9c843c8c359fd531c62509bf5d9c30aa1f693a
shreshth-06 <shreshthchaturvedi2401@gmail.com>
2021-12-15 16:10:38 +0530

    added light/dark mode button for the About us page using useState hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About Us" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" />
+        {/* <TextForm heading="Enter the text to analyze" /> */}
+        <About />
       </div>
     </>
   );
