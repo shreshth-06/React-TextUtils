b86ac8a30a9fa2ee97a0585aaee6b7d2904a224d
shreshth-06 <shreshthchaturvedi2401@gmail.com>
2021-12-16 13:20:52 +0530

    Updates

---
+++ precache-manifest.83b8669425c66be75d4750570c7950f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1b14c1380c16d460df1",
+    "url": "/React-TextUtils/static/css/main.f4822cd0.chunk.css"
+  },
+  {
+    "revision": "a1b14c1380c16d460df1",
+    "url": "/React-TextUtils/static/js/main.a1b14c13.chunk.js"
+  },
+  {
+    "revision": "127796a22acc74b336fe",
+    "url": "/React-TextUtils/static/js/1.127796a2.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/React-TextUtils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "304c22fc10c8327d8a7c",
+    "url": "/React-TextUtils/static/js/runtime~main.304c22fc.js"
+  },
+  {
+    "revision": "f4999a2188a246455cbf2728737f3002",
+    "url": "/React-TextUtils/index.html"
+  }
+];
